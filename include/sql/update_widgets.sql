a67f5f3c810b9a8835ef863b86d6cbd36b0bb9be
naoki hirata <naoki@magic3.org>
2013-09-03 15:40:02 +0900

    ウィジェット情報更新。

--- include/sql/update_widgets.sql
+++ include/sql/update_widgets.sql
@@ -386,7 +386,11 @@ INSERT INTO _widgets
 DELETE FROM _widgets WHERE wd_id = 'event_category_menu';
 INSERT INTO _widgets
 (wd_id,                 wd_name,                         wd_version, wd_author,      wd_copyright, wd_license, wd_official_level, wd_description, wd_has_admin, wd_enable_operation, wd_use_instance_def, wd_initialized, wd_launch_index, wd_cache_type, wd_view_control_type, wd_release_dt, wd_install_dt, wd_create_dt) VALUES
-('event_category_menu', 'イベント - カテゴリーメニュー', '1.0.0',    'Naoki Hirata', 'Magic3.org', 'GPL', 10, 'イベントカテゴリ一覧を表示。',     false,         false,               false,true,           0, 1, -1, '2013-09-02', now(), now());
+('event_category_menu', 'イベント情報 - カテゴリーメニュー', '1.0.0',    'Naoki Hirata', 'Magic3.org', 'GPL', 10, 'イベントカテゴリ一覧を表示。',     false,         false,               false,true,           0, 1, -1, '2013-09-02', now(), now());
+DELETE FROM _widgets WHERE wd_id = 'event_category';
+INSERT INTO _widgets
+(wd_id,            wd_name,                     wd_version, wd_author,      wd_copyright, wd_license, wd_official_level, wd_description,             wd_has_admin, wd_enable_operation, wd_use_instance_def, wd_initialized, wd_cache_type, wd_view_control_type, wd_has_rss, wd_release_dt, wd_install_dt, wd_create_dt) VALUES
+('event_category', 'イベント情報 - 選択カテゴリー', '1.0.0',    'Naoki Hirata', 'Magic3.org', 'GPL',      10,                'イベント記事の1カテゴリに属する記事の一覧を表示。', true,        false,               true,true,               1, -1, true, '2013-09-02', now(),    now());
 
 -- ウィジェット情報(携帯用)
 DELETE FROM _widgets WHERE wd_id = 'm/mainmenu';
