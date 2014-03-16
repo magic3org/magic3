19f42d87474bb02dd0c526d25e63d50670d0b51b
naoki hirata <naoki@magic3.org>
2014-03-16 22:11:44 +0900

    テンプレート「bootstrap_yeti」更新。

--- templates/bootstrap_yeti/index.php
+++ templates/bootstrap_yeti/index.php
@@ -21,6 +21,9 @@ $templateUrl = $document->baseurl . '/templates/' . $document->template;
     <jdoc:include type="head" />
 	<link rel="stylesheet" href="<?php echo $templateUrl; ?>/css/bootswatch_yeti_ja.min.css" media="screen">
 	<link rel="stylesheet" href="<?php echo $templateUrl; ?>/css/style.css" media="screen">
+	<?php global $gPageManager;if ($gPageManager->isLayout()): ?>
+	<link rel="stylesheet" href="<?php echo $templateUrl; ?>/css/style_layout.css" media="screen">
+	<?php endif; ?>
 	<!--[if lt IE 9]>
 	<script src="<?php echo $templateUrl; ?>/html5shiv.js"></script>
 	<script src="<?php echo $templateUrl; ?>/respond.min.js"></script>
@@ -38,20 +41,21 @@ $(function(){
 <jdoc:include type="modules" name="user3" />
 <div class="hidden-xs"><jdoc:include type="modules" name="top-hide" style="none" /></div>
 <div class="row">
-<?php if ($document->countModules('left')): ?>
+<?php if ($document->countModules('left') || $document->countModules('left-static') || $document->countModules('left-hide')): ?>
     <div class="col-sm-3"><div class="row">
-	<div class="col-sm-12"><jdoc:include type="modules" name="left" style="none" /></div>
-	<div class="col-sm-12 hidden-xs"><jdoc:include type="modules" name="left-hide" style="none" /></div>
+	<?php if ($document->countModules('left-static')): ?><div class="col-sm-12 hidden-xs"><div class="pos-static"><jdoc:include type="modules" name="left-static" style="none" /></div></div><?php endif; ?>
+	<?php if ($document->countModules('left')): ?><div class="col-sm-12"><jdoc:include type="modules" name="left" style="none" /></div><?php endif; ?>
+	<?php if ($document->countModules('left-hide')): ?><div class="col-sm-12 hidden-xs"><jdoc:include type="modules" name="left-hide" style="none" /></div><?php endif; ?>
 	</div></div>
-<?php endif; ?>
-<?php if ($document->countModules('left')): ?>
     <div class="col-sm-9"><div class="row">
-	<div class="col-sm-12"><jdoc:include type="modules" name="banner" style="none" /></div>
+	<?php if ($document->countModules('banner')): ?><div class="col-sm-12"><jdoc:include type="modules" name="banner" style="none" /></div><?php endif; ?>
 	<div class="col-sm-12"><jdoc:include type="component" style="none" /></div>
-	<div class="col-sm-12 hidden-xs"><jdoc:include type="modules" name="center-hide" style="none" /></div>
+	<?php if ($document->countModules('center-hide')): ?><div class="col-sm-12 hidden-xs"><jdoc:include type="modules" name="center-hide" style="none" /></div><?php endif; ?>
 	</div></div>
 <?php else: ?>
-    <jdoc:include type="component" style="none" />
+	<?php if ($document->countModules('banner')): ?><div class="col-sm-12"><jdoc:include type="modules" name="banner" style="none" /></div><?php endif; ?>
+	<div class="col-sm-12"><jdoc:include type="component" style="none" /></div>
+	<?php if ($document->countModules('center-hide')): ?><div class="col-sm-12 hidden-xs"><jdoc:include type="modules" name="center-hide" style="none" /></div><?php endif; ?>
 <?php endif; ?>
 </div>
 <jdoc:include type="modules" name="bottom" style="none" />
