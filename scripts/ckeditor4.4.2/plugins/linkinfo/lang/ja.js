c86d0c0129f9c252c3b7d7c28d3e057defb585d1
naoki hirata <naoki@magic3.org>
2014-07-08 09:41:05 +0900

    新規CKEditorにプラグイン移動。

---
+++ scripts/ckeditor4.4.2/plugins/linkinfo/lang/ja.js
@@ -0,0 +1,28 @@
+/**
+ * Magic3 CKEditorプラグイン
+ *
+ * JavaScript 1.5
+ *
+ * LICENSE: This source file is licensed under the terms of the GNU General Public License.
+ *
+ * @package    Magic3 Framework
+ * @author     平田直毅(Naoki Hirata) <naoki@aplo.co.jp>
+ * @copyright  Copyright 2006-2013 Magic3 Project.
+ * @license    http://www.gnu.org/copyleft/gpl.html  GPL License
+ * @version    SVN: $Id$
+ * @link       http://www.magic3.org
+ */
+CKEDITOR.plugins.setLang( 'linkinfo', 'ja', {
+	toolbar: 'Magic3 URL作成',
+	title: 'Magic3 URL作成',
+	tab_info_title: 'URL情報',
+	tab_advanced_title: '高度な設定',
+	link_target_title: 'リンク対象を選択してください。',
+	page_list_title: 'ページ',
+	content_type_title: 'コンテンツ種別',
+	content_list_title: 'コンテンツ',
+	access_point_title: 'アクセスポイント',
+	url_title: 'URL',
+	content_title: 'タイトル',
+	content_label_title: 'コンテンツ内容：'
+});
