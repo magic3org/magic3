5f3524804f9a509fda01d713b0128dfa0dfb3573
naoki hirata <naoki@magic3.org>
2014-07-25 22:10:27 +0900

    DB更新スクリプト追加。

---
+++ include/sql/update/2014072101_to_2014072501.sql
@@ -0,0 +1,27 @@
+-- *
+-- * バージョンアップ用スクリプト
+-- *
+-- * PHP versions 5
+-- *
+-- * LICENSE: This source file is licensed under the terms of the GNU General Public License.
+-- *
+-- * @package    Magic3 Framework
+-- * @author     平田直毅(Naoki Hirata) <naoki@aplo.co.jp>
+-- * @copyright  Copyright 2006-2014 Magic3 Project.
+-- * @license    http://www.gnu.org/copyleft/gpl.html  GPL License
+-- * @version    SVN: $Id$
+-- * @link       http://www.magic3.org
+-- *
+-- --------------------------------------------------------------------------------------------------
+-- バージョンアップ用スクリプト
+-- --------------------------------------------------------------------------------------------------
+
+-- *** システムベーステーブル ***
+-- 追加クラスマスター
+
+INSERT INTO _addons
+(ao_id,     ao_class_name, ao_name,            ao_description, ao_opelog_hook) VALUES
+('wikilib', 'wikiLib',     'Wikiライブラリ', '', false);
+
+
+-- *** システム標準テーブル ***
