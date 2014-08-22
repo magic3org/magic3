518deecc757bbb6ed0c1b74a3fca11c9fc945418
naoki hirata <naoki@magic3.org>
2014-08-22 09:35:40 +0900

    コンテンツテンプレート用ディレクトリ追加。

---
+++ include/ctemplate/info.js
@@ -0,0 +1,15 @@
+/**
+ * コンテンツテンプレートディレクトリ情報
+ *
+ * JavaScript 1.5
+ *
+ * LICENSE: This source file is licensed under the terms of the GNU General Public License.
+ *
+ * @package    Magic3 Framework
+ * @author     平田直毅(Naoki Hirata) <naoki@aplo.co.jp>
+ * @copyright  Copyright 2006-2014 Magic3 Project.
+ * @license    http://www.gnu.org/copyleft/gpl.html  GPL License
+ * @version    1.0
+ * @link       http://www.magic3.org
+ */
+var content_template_info = [ 'test01', 'test02' ]
