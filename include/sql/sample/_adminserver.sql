741e8ab034820c538d8eacee0a66a6156f5b9d0d
naoki hirata <naoki@magic3.org>
2014-10-18 20:54:21 +0900

    サーバ管理用メニューデータ追加。

---
+++ include/sql/sample/_adminserver.sql
@@ -0,0 +1,29 @@
+-- *
+-- * データ登録スクリプト「サーバ管理」
+-- *
+-- * PHP versions 5
+-- *
+-- * LICENSE: This source file is licensed under the terms of the GNU General Public License.
+-- *
+-- * @package    Magic3 Framework
+-- * @author     平田直毅(Naoki Hirata) <naoki@aplo.co.jp>
+-- * @copyright  Copyright 2006-2014 Magic3 Project.
+-- * @license    http://www.gnu.org/copyleft/gpl.html  GPL License
+-- * @version    SVN: $Id$
+-- * @link       http://www.magic3.org
+-- *
+-- [サーバ管理]
+-- サーバ管理を行う。
+
+-- 管理画面メニューデータ
+DELETE FROM _nav_item;
+INSERT INTO _nav_item
+(ni_id, ni_parent_id, ni_index, ni_nav_id,    ni_task_id,        ni_view_control, ni_param, ni_name,                ni_help_title,          ni_help_body) VALUES
+(200,   0,            2,        'admin_menu', '_adminserver',    0,               '',       'システム運用',         '',                     ''),
+(201,   200,          0,        'admin_menu', 'serverinfo',      0,               '',       'サーバ情報',           'サーバ情報',           'サーバについての情報'),
+(202,   200,          1,        'admin_menu', 'hostlist',        0,               '',       'ホスト一覧', 'ホスト一覧', 'ホスト一覧'),
+(299,   0,            3,        'admin_menu', '_299',            1,               '',       '改行',                 '',                     ''),
+(300,   0,            4,        'admin_menu', '_config',         0,               '',       'システム管理',         '',                     ''),
+(301,   300,          0,        'admin_menu', 'configsite',      0,               '',       '基本情報',             '基本情報',             'サイト運営に必要な情報を設定します。'),
+(302,   300,          1,        'admin_menu', 'configsys',       0,               '',       'システム情報',         'システム情報',         'システム全体の設定、運用状況を管理します。'),
+(303,   300,          2,        'admin_menu', 'mainte',          0,               '',       'メンテナンス', 'メンテナンス', 'ファイルやDBなどのメンテナンスを行います。');
