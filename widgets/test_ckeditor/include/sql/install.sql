65ff105a5675931537ded6a5f5d7addcf10de434
naoki hirata <naoki@magic3.org>
2015-01-25 18:00:20 +0900

    CKEditorテスト用ウィジェット更新。

---
+++ widgets/test_ckeditor/include/sql/install.sql
@@ -0,0 +1,6 @@
+-- テスト用テーブル
+DELETE FROM _widgets WHERE wd_id = 'test_ckeditor';
+INSERT INTO _widgets
+(wd_id,          wd_name,              wd_version, wd_author,      wd_copyright, wd_license, wd_official_level, wd_description,         wd_add_script_lib_a, wd_install_dt, wd_create_dt) VALUES
+('test_ckeditor', 'CKEditorテスト', '1.1.0',    'Naoki Hirata', 'Magic3.org', 'GPL',      10,                'CKEditorテスト用', 'elfinder', now(), now());
+
