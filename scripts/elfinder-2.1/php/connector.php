99eb1feb5db62818f86d74c237bccbc876adbb01
naoki hirata <naoki@magic3.org>
2015-01-26 13:55:51 +0900

    elFinder2.1更新。

--- scripts/elfinder-2.1/php/connector.php
+++ scripts/elfinder-2.1/php/connector.php
@@ -1,6 +1,29 @@
 <?php
+/**
+ * index.php
+ *
+ * PHP versions 5
+ *
+ * LICENSE: This source file is licensed under the terms of the GNU General Public License.
+ *
+ * @package    Magic3 Framework
+ * @author     平田直毅(Naoki Hirata) <naoki@aplo.co.jp>
+ * @copyright  Copyright 2006-2014 Magic3 Project.
+ * @license    http://www.gnu.org/copyleft/gpl.html  GPL License
+ * @version    SVN: $Id$
+ * @link       http://www.magic3.org
+ */
+// ########## Magic3アクセス制御(開始) ##########
+require_once('../../../include/global.php');
 
-error_reporting(0); // Set E_ALL for debuging
+if (!$gAccessManager->loginedByUser()){		// ログイン中のユーザはアクセスを許可
+	echo 'Access error: access denied.';
+
+	$gOpeLogManager->writeUserAccess(__METHOD__, 'ファイルブラウザへの不正なアクセスを検出しました。ログインなし', 3001, 'アクセスをブロックしました。');
+	exit(0);
+}
+// ########## Magic3アクセス制御(終了) ##########
+//error_reporting(0); // Set E_ALL for debuging
 
 include_once dirname(__FILE__).DIRECTORY_SEPARATOR.'elFinderConnector.class.php';
 include_once dirname(__FILE__).DIRECTORY_SEPARATOR.'elFinder.class.php';
@@ -40,6 +63,23 @@ function access($attr, $path, $data, $volume) {
 		:  null;                                    // else elFinder decide it itself
 }
 
+// ########## Magic3アクセス制御(開始) ##########
+// ディレクトリ参照範囲を制限
+$dirType = $gRequestManager->trimValueOf('dirtype');
+if (!empty($dirType) && !in_array($dirType, array('image', 'flash', 'file'))){
+	$gOpeLogManager->writeUserAccess(__METHOD__, 'ファイルブラウザへの不正なパラメータを検出しました。dirtype=' . $dirType , 3001, 'アクセスをブロックしました。');
+	exit(0);
+}
+$path = $gEnvManager->getResourcePathForUser();
+$url = $gEnvManager->getRelativeResourcePathToDocumentRootForUser();
+if (!empty($dirType)){
+	$path .= '/' . $dirType;
+	$url .= '/' . $dirType;
+}
+
+// ディレクトリがない場合は作成
+if (!file_exists($path)) mkdir($path, M3_SYSTEM_DIR_PERMISSION, true/*再帰的*/);
+// ########## Magic3アクセス制御(終了) ##########
 
 // Documentation for connector options:
 // https://github.com/Studio-42/elFinder/wiki/Connector-configuration-options
@@ -48,8 +88,10 @@ $opts = array(
 	'roots' => array(
 		array(
 			'driver'        => 'LocalFileSystem',   // driver for accessing file system (REQUIRED)
-			'path'          => '../files/',         // path to files (REQUIRED)
-			'URL'           => dirname($_SERVER['PHP_SELF']) . '/../files/', // URL to files (REQUIRED)
+//			'path'          => '../files/',         // path to files (REQUIRED)
+//			'URL'           => dirname($_SERVER['PHP_SELF']) . '/../files/', // URL to files (REQUIRED)
+			'path'          => $path,		// path to files (REQUIRED)
+			'URL'           => $url,		// URL to files (REQUIRED)
 			'accessControl' => 'access'             // disable and hide dot starting files (OPTIONAL)
 		)
 	)
