f148f2072e3833c77ad0bc71884517a28c0dd2aa
naoki hirata <naoki@magic3.org>
2015-01-26 15:43:31 +0900

    旧Bootstrap v3.1.1を削除。

--- include/common/scriptLibInfo.php
+++ include/common/scriptLibInfo.php
@@ -284,12 +284,9 @@ const JQUERY_JQPLOT_CSS				= 'jquery/jqplot1.0.8/jquery.jqplot.min.css';
 	const SWFOBJECT_FILENAME		= 'swfobject/swfobject.js';					// swfobject
 
 	// Bootstrapライブラリ
-//	const BOOTSTRAP_FILENAME		= 'bootstrap-3.1.1/js/bootstrap.min.js';			// bootstrap
-//	const BOOTSTRAP_CSS				= 'bootstrap-3.1.1/css/bootstrap.min.css';
 	const BOOTSTRAP_FILENAME		= 'bootstrap-3.2.0/js/bootstrap.min.js';			// bootstrap
 	const BOOTSTRAP_CSS				= 'bootstrap-3.2.0/css/bootstrap.min.css';
 	// BootstrapオプションはBootstrapのディレクトリ配下に格納
-//	const BOOTSTRAP_BOOTSWATCH_FLATLY_CSS			= 'bootstrap-3.1.1/css/bootswatch_flatly_ja.css';	// bootstrap配色(日本語フォント)
 	const BOOTSTRAP_BOOTSWATCH_FLATLY_CSS			= 'bootstrap-3.2.0/css/bootswatch_flatly_ja.css';	// bootstrap配色(日本語フォント)
 	const BOOTSTRAP_BOOTSNIPP_LARGEDROPDOWNMENU_CSS = 'bootstrap/css/bootsnipp_largedropdownmenu.css';
 	const BOOTSTRAP_YAMM_CSS						= 'bootstrap/css/yamm.css';					// bootstrapメガメニュー
