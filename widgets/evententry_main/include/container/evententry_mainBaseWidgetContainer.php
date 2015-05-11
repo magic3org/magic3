6d30a8e528e141e07a2ca4bad6ad458c5356eb1a
naoki hirata <naoki@magic3.org>
2015-05-11 15:46:37 +0900

    「イベント予約メイン」ウィジェット更新。

---
+++ widgets/evententry_main/include/container/evententry_mainBaseWidgetContainer.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * index.php用コンテナクラス
+ *
+ * PHP versions 5
+ *
+ * LICENSE: This source file is licensed under the terms of the GNU General Public License.
+ *
+ * @package    Magic3 Framework
+ * @author     平田直毅(Naoki Hirata) <naoki@aplo.co.jp>
+ * @copyright  Copyright 2006-2013 Magic3 Project.
+ * @license    http://www.gnu.org/copyleft/gpl.html  GPL License
+ * @version    SVN: $Id$
+ * @link       http://www.magic3.org
+ */
+require_once($gEnvManager->getContainerPath() . '/baseWidgetContainer.php');
+require_once($gEnvManager->getCurrentWidgetContainerPath() . '/evententry_mainCommonDef.php');
+require_once($gEnvManager->getCurrentWidgetDbPath() . '/evententry_mainDb.php');
+
+class evententry_mainBaseWidgetContainer extends BaseWidgetContainer
+{
+	protected static $_mainDb;			// DB接続オブジェクト
+	protected static $_configArray;		// イベント定義値
+	protected static $_paramObj;		// ウィジェットパラメータオブジェクト
+	protected static $_canEditEntry;	// 記事が編集可能かどうか
+	protected $_pageUrl;		// 現在のページのURL
+	protected $_baseUrl;		// ベースURL
+	protected $_useCalendar;		// カレンダーを使用するかどうか
+	const DATE_RANGE_DELIMITER		= '～';				// 日時範囲用デリミター
+	const CSS_FILE = '/style.css';		// CSSファイルのパス
+
+	// 画面
+	const TASK_TOP			= 'top';			// トップ画面
+	const TASK_CALENDAR		= 'calendar';		// カレンダー画面
+	const DEFAULT_TASK		= 'top';
+	
+	// カレンダー用スクリプト
+	const CALENDAR_SCRIPT_FILE = '/jscalendar-1.0/calendar.js';		// カレンダースクリプトファイル
+	const CALENDAR_LANG_FILE = '/jscalendar-1.0/lang/calendar-ja.js';	// カレンダー言語ファイル
+	const CALENDAR_SETUP_FILE = '/jscalendar-1.0/calendar-setup.js';	// カレンダーセットアップファイル
+	const CALENDAR_CSS_FILE = '/jscalendar-1.0/calendar-win2k-1.css';		// カレンダー用CSSファイル
+		
+	/**
+	 * コンストラクタ
+	 */
+	function __construct()
+	{
+		// 親クラスを呼び出す
+		parent::__construct();
+		
+		// サブウィジェット起動のときだけ初期処理実行
+		if ($this->gEnv->getIsSubWidget()){
+			// DBオブジェクト作成
+			if (!isset(self::$_mainDb)) self::$_mainDb = new evententry_mainDb();
+		
+			// イベント定義を読み込む
+			if (!isset(self::$_configArray)) self::$_configArray = event_mainCommonDef::loadConfig(self::$_mainDb);
+		}
+	}
+	/**
+	 * ウィジェット初期化
+	 *
+	 * 共通パラメータの初期化や、以下のパターンでウィジェット出力方法の変更を行う。
+	 * ・組み込みの_setTemplate(),_assign()を使用
+	 *
+	 * @param RequestManager $request		HTTPリクエスト処理クラス
+	 * @return 								なし
+	 */
+	function _preInit($request)
+	{
+		// URLパラメータ取得
+//		$this->_blogId = $request->trimValueOf(M3_REQUEST_PARAM_BLOG_ID);		// 所属ブログ
+		$this->addOptionUrlParam(M3_REQUEST_PARAM_OPEN_BY, $this->_openBy);		// ウィンドウオープンタイプ
+//		$this->addOptionUrlParam(M3_REQUEST_PARAM_BLOG_ID, $this->_blogId);
+		
+		// 共通パラメータ初期化
+		$this->_pageUrl = $this->gEnv->createCurrentPageUrl();		// 現在のページのURL
+		$this->_baseUrl = $this->getUrlWithOptionParam();			// ベースURL(オプション付き)
+		
+		$this->_useCalendar	= self::$_configArray[event_mainCommonDef::CF_USE_CALENDAR];		// カレンダーを使用するかどうか
+	}
+	/**
+	 * テンプレートに前処理
+	 *
+	 * _setTemplate()で指定したテンプレートファイルにデータを埋め込む。
+	 *
+	 * @param RequestManager $request		HTTPリクエスト処理クラス
+	 * @param object         $param			任意使用パラメータ。_setTemplate()と共有。
+	 * @return								なし
+	 */
+	function _preAssign($request, &$param)
+	{
+	}
+	/**
+	 * テンプレートにデータ埋め込む
+	 *
+	 * _setTemplate()で指定したテンプレートファイルにデータを埋め込む。
+	 *
+	 * @param RequestManager $request		HTTPリクエスト処理クラス
+	 * @param object         $param			任意使用パラメータ。_setTemplate()と共有。
+	 * @return								なし
+	 */
+	function _postAssign($request, &$param)
+	{
+	}
+	/**
+	 * CSSデータをHTMLヘッダ部に設定
+	 *
+	 * CSSデータをHTMLのheadタグ内に追加出力する。(注意)別ファイル出力はページ単位のURLなのでタスクに関係なく出力する必要がある。
+	 * _assign()よりも後に実行される。
+	 *
+	 * @param RequestManager $request		HTTPリクエスト処理クラス
+	 * @param object         $param			任意使用パラメータ。
+	 * @return string 						CSS文字列。出力しない場合は空文字列を設定。
+	 */
+	function _addCssToHead($request, &$param)
+	{
+		// CSSを作成
+		$css = '';
+		if ($this->_useCalendar) $css = $this->getParsedTemplateData('calendar.tmpl.css');
+		return $css;
+	}
+	/**
+	 * CSSファイルをHTMLヘッダ部に設定
+	 *
+	 * CSSファイルをHTMLのheadタグ内に追加出力する。
+	 * _assign()よりも後に実行される。
+	 *
+	 * @param RequestManager $request		HTTPリクエスト処理クラス
+	 * @param object         $param			任意使用パラメータ。
+	 * @return string 						CSS文字列。出力しない場合は空文字列を設定。
+	 */
+	function _addCssFileToHead($request, &$param)
+	{
+//		if ($this->_renderType == M3_RENDER_BOOTSTRAP){
+//			return '';
+//		} else {
+			return $this->getUrl($this->gEnv->getCurrentWidgetCssUrl() . self::CSS_FILE);		// デフォルトのCSSファイル
+//		}
+	}
+}
+?>
