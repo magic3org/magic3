f3c1e8f24575f2d24be4edb150a75283cb7a6ab9
naoki hirata <naoki@magic3.org>
2015-05-12 09:32:59 +0900

    ウィジェット情報更新。

--- include/sql/update_widgets.sql
+++ include/sql/update_widgets.sql
@@ -243,8 +243,8 @@ INSERT INTO _widgets
 ('default_login_box', 'デフォルトログイン', 'member',       'bootstrap',      '2.2.0',  'Naoki Hirata', 'Magic3.org', 'GPL', 10, '汎用の会員ログイン用ウィジェットです。「汎用会員登録」ウィジェットと連携できます。', 'md5',        true,         true,        true,        false,               false,true,               0, 2, -1, '2014-09-26', now(),    now());
 DELETE FROM _widgets WHERE wd_id = 'reg_user';
 INSERT INTO _widgets
-(wd_id,      wd_name,          wd_content_type, wd_category_id, wd_version, wd_author,      wd_copyright, wd_license, wd_official_level, wd_description,           wd_add_script_lib, wd_has_admin, wd_enable_operation, wd_initialized, wd_launch_index, wd_cache_type, wd_view_control_type, wd_release_dt, wd_install_dt, wd_create_dt) VALUES
-('reg_user', '汎用会員登録',  'member', 'member',      '2.0.1',    'Naoki Hirata', 'Magic3.org', 'GPL',      10,                '汎用的な会員登録機能です。「デフォルトログイン」ウィジェットと連携できます。', 'md5', true, true,        true,           0, 0,              0, '2012-09-19', now(), now());
+(wd_id,      wd_name,        wd_content_type, wd_category_id, wd_version, wd_author,      wd_copyright, wd_license, wd_official_level, wd_description,           wd_add_script_lib, wd_has_admin, wd_enable_operation, wd_initialized, wd_release_dt, wd_install_dt, wd_create_dt) VALUES
+('reg_user', '汎用会員登録', 'member',        'member',       '2.0.2',    'Naoki Hirata', 'Magic3.org', 'GPL',      10,                '汎用的な会員登録機能です。「デフォルトログイン」ウィジェットと連携できます。', 'md5', true, true,        true,           '2015-05-12', now(), now());
 -- ウィジェット情報(PC用/画像)
 DELETE FROM _widgets WHERE wd_id = 'pretty_photo';
 INSERT INTO _widgets
