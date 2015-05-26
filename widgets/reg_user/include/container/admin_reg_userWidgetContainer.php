4dd4c4d6c66d8b12d443649e0f85dbaae83fab0d
naoki hirata <naoki@magic3.org>
2015-05-26 15:54:52 +0900

    「汎用会員登録」ウィジェット更新。

--- widgets/reg_user/include/container/admin_reg_userWidgetContainer.php
+++ widgets/reg_user/include/container/admin_reg_userWidgetContainer.php
@@ -58,8 +58,16 @@ class admin_reg_userWidgetContainer extends BaseAdminWidgetContainer
 	 */
 	function _assign($request, &$param)
 	{
-		$act = $request->trimValueOf('act');
+		$act			= $request->trimValueOf('act');
 		$this->authType = $request->trimValueOf('item_auth_type');		// 承認タイプ
+		$titleRegistAuto			= $request->trimValueOf('item_title_regist_auto');					// メールテンプレートタイトル(会員自動登録)
+		$contentRegistAuto			= $request->trimValueOf('item_content_regist_auto');				// メールテンプレート本文(会員自動登録)
+		$titleRegistAutoCompleted	= $request->trimValueOf('item_title_regist_auto_completed');		// メールテンプレートタイトル(会員自動登録完了)
+		$contentRegistAutoCompleted	= $request->trimValueOf('item_content_regist_auto_completed');		// メールテンプレート本文(会員自動登録完了)
+		$titleRegistAuth			= $request->trimValueOf('item_title_regist_auth');					// メールテンプレートタイトル(会員承認登録)
+		$contentRegistAuth			= $request->trimValueOf('item_content_regist_auth');				// メールテンプレート本文(会員承認登録)
+		$titleRegistAuthCompleted	= $request->trimValueOf('item_title_regist_auth_completed');		// メールテンプレートタイトル(会員承認登録完了)
+		$contentRegistAuthCompleted	= $request->trimValueOf('item_content_regist_auth_completed');		// メールテンプレート本文(会員承認登録完了)
 		
 		$replaceNew = false;		// データを再取得するかどうか
 		if ($act == 'update'){		// 設定更新のとき
@@ -71,7 +79,10 @@ class admin_reg_userWidgetContainer extends BaseAdminWidgetContainer
 				$paramObj->authType	= $this->authType;		// 承認タイプ
 				$ret = $this->updateWidgetParamObj($paramObj);
 				
-				if ($ret) 		$ret = $this->gInstance->getMailManager()->updateMailForm($formId, $this->_langId, $content, $subject);
+				if ($ret) $ret = $this->_updateMailForm(reg_userCommonDef::MAIL_TMPL_REGIST_USER_AUTO, $titleRegistAuto, $contentRegistAuto);		// メールテンプレート(会員自動登録)
+				if ($ret) $ret = $this->_updateMailForm(reg_userCommonDef::MAIL_TMPL_REGIST_USER_AUTO_COMPLETED, $titleRegistAutoCompleted, $contentRegistAutoCompleted);		// メールテンプレート(会員自動登録完了)
+				if ($ret) $ret = $this->_updateMailForm(reg_userCommonDef::MAIL_TMPL_REGIST_USER_AUTH, $titleRegistAuth, $contentRegistAuth);		// メールテンプレート(会員承認登録)
+				if ($ret) $ret = $this->_updateMailForm(reg_userCommonDef::MAIL_TMPL_REGIST_USER_AUTH_COMPLETED, $titleRegistAuthCompleted, $contentRegistAuthCompleted);		// メールテンプレート(会員承認登録完了)
 				
 				if ($ret){
 					$this->setMsg(self::MSG_GUIDANCE, 'データを更新しました');
@@ -92,10 +103,25 @@ class admin_reg_userWidgetContainer extends BaseAdminWidgetContainer
 			if (!empty($paramObj)){
 				$this->authType	= $paramObj->authType;			// 承認タイプ
 			}
+			
+			list($titleRegistAuto, $contentRegistAuto)						= $this->_getMailForm(reg_userCommonDef::MAIL_TMPL_REGIST_USER_AUTO);					// メールテンプレート(会員自動登録)
+			list($titleRegistAutoCompleted, $contentRegistAutoCompleted)	= $this->_getMailForm(reg_userCommonDef::MAIL_TMPL_REGIST_USER_AUTO_COMPLETED);			// メールテンプレート(会員自動登録完了)
+			list($titleRegistAuth, $contentRegistAuth)						= $this->_getMailForm(reg_userCommonDef::MAIL_TMPL_REGIST_USER_AUTH);					// メールテンプレート(会員承認登録)
+			list($titleRegistAuthCompleted, $contentRegistAuthCompleted)	= $this->_getMailForm(reg_userCommonDef::MAIL_TMPL_REGIST_USER_AUTH_COMPLETED);			// メールテンプレート(会員承認登録完了)
 		}
 		
 		// 承認タイプ選択肢作成
 		$this->createAuthType();
+		
+		// 値を画面に埋め込む
+		$this->tmpl->addVar("_widget", "title_regist_auto",	$this->convertToDispString($titleRegistAuto));		// メールテンプレートタイトル(会員自動登録)
+		$this->tmpl->addVar("_widget", "content_regist_auto",	$this->convertToDispString($contentRegistAuto));	// メールテンプレート本文(会員自動登録)
+		$this->tmpl->addVar("_widget", "title_regist_auto_completed",	$this->convertToDispString($titleRegistAutoCompleted));			// メールテンプレートタイトル(会員自動登録完了)
+		$this->tmpl->addVar("_widget", "content_regist_auto_completed",	$this->convertToDispString($contentRegistAutoCompleted));	// メールテンプレート本文(会員自動登録完了)
+		$this->tmpl->addVar("_widget", "title_regist_auth",	$this->convertToDispString($titleRegistAuth));			// メールテンプレートタイトル(会員承認登録)
+		$this->tmpl->addVar("_widget", "content_regist_auth",	$this->convertToDispString($contentRegistAuth));		// メールテンプレート本文(会員承認登録)
+		$this->tmpl->addVar("_widget", "title_regist_auth_completed",	$this->convertToDispString($titleRegistAuthCompleted));			// メールテンプレートタイトル(会員承認登録完了)
+		$this->tmpl->addVar("_widget", "content_regist_auth_completed",	$this->convertToDispString($contentRegistAuthCompleted));	// メールテンプレート本文(会員承認登録完了)
 	}
 	/**
 	 * テンプレートにデータ埋め込む
@@ -133,6 +159,37 @@ class admin_reg_userWidgetContainer extends BaseAdminWidgetContainer
 			$this->tmpl->addVars('authtype_list', $row);
 			$this->tmpl->parseTemplate('authtype_list', 'a');
 		}
+	}
+	/**
+	 * メールフォームデータを取得
+	 *
+	 * @param string $formId		フォームID
+	 * @return array 				タイトル、フォームデータの配列
+	 */
+	function _getMailForm($formId)
+	{
+		$title = '';
+		$content = '';
+		
+		$ret = $this->gInstance->getMailManager()->getMailForm($formId, $this->_langId, $row);
+		if ($ret){
+			$title		= $row['mf_subject'];
+			$content	= $row['mf_content'];
+		}
+		return array($title, $content);
+	}
+	/**
+	 * メールフォームデータを更新
+	 *
+	 * @param string $formId		フォームID
+	 * @param string $title			メールタイトル
+	 * @param string $content		メール本文
+	 * @return bool 				true=成功、false=失敗
+	 */
+	function _updateMailForm($formId, $title, $content)
+	{
+		$ret = $this->gInstance->getMailManager()->updateMailForm($formId, $this->_langId, $content, $title);
+		return $ret;
 	}
 }
 ?>
