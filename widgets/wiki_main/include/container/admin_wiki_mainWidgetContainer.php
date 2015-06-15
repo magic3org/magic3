9cf7e21895973d2600e95956167ed43af7b0a3e0
naoki hirata <naoki@magic3.org>
2015-06-15 12:11:08 +0900

    「wikiメイン」ウィジェット更新。

--- widgets/wiki_main/include/container/admin_wiki_mainWidgetContainer.php
+++ widgets/wiki_main/include/container/admin_wiki_mainWidgetContainer.php
@@ -8,7 +8,7 @@
  *
  * @package    Magic3 Framework
  * @author     平田直毅(Naoki Hirata) <naoki@aplo.co.jp>
- * @copyright  Copyright 2006-2014 Magic3 Project.
+ * @copyright  Copyright 2006-2015 Magic3 Project.
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL License
  * @version    SVN: $Id$
  * @link       http://www.magic3.org
@@ -72,6 +72,20 @@ class admin_wiki_mainWidgetContainer extends BaseAdminWidgetContainer
 	{
 		// 詳細設定画面
 		return $this->createDetail($request);
+	}
+	/**
+	 * テンプレートにデータ埋め込む
+	 *
+	 * _setTemplate()で指定したテンプレートファイルにデータを埋め込む。
+	 *
+	 * @param RequestManager $request		HTTPリクエスト処理クラス
+	 * @param object         $param			任意使用パラメータ。_setTemplate()と共有。
+	 * @return								なし
+	 */
+	function _postAssign($request, &$param)
+	{
+		// メニューバー、パンくずリスト作成(簡易版)
+		$this->createBasicConfigMenubar($request);
 	}
 	/**
 	 * 詳細画面作成
