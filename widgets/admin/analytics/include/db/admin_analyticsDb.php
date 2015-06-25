6f89645b99759bd34ca5041102a0d16d9427273c
naoki hirata <naoki@magic3.org>
2015-06-25 11:34:57 +0900

    「アクセス解析」ウィジェットのDBアクセスをチューニング。

--- widgets/admin/analytics/include/db/admin_analyticsDb.php
+++ widgets/admin/analytics/include/db/admin_analyticsDb.php
@@ -28,9 +28,18 @@ class admin_analyticsDb extends BaseDb
 	function getOldAccessLog(&$row)
 	{
 //		$queryStr  = 'SELECT * FROM _access_log ';
-		$queryStr  = 'SELECT al_dt FROM _access_log ';
+/*		$queryStr  = 'SELECT al_dt FROM _access_log ';
 		$queryStr .=   'ORDER BY al_serial';
+		$ret = $this->selectRecord($queryStr, array(), $row);*/
+		
+		$serial = 0;
+		$queryStr  = 'SELECT min(al_serial) as m FROM _access_log ';
 		$ret = $this->selectRecord($queryStr, array(), $row);
+		if ($ret) $serial = $row['m'];
+		
+		$queryStr  = 'SELECT * FROM _access_log ';
+		$queryStr .=   'WHERE al_serial = ?';
+		$ret = $this->selectRecord($queryStr, array($serial), $row);
 		return $ret;
 	}
 	/**
