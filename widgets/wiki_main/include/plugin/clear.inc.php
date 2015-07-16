ca96b215076b45375facfa2af81e132bd0560059
naoki hirata <naoki@magic3.org>
2015-07-16 18:02:18 +0900

    「wikiメイン」ウィジェット更新。

--- widgets/wiki_main/include/plugin/clear.inc.php
+++ widgets/wiki_main/include/plugin/clear.inc.php
@@ -8,15 +8,15 @@
  *
  * @package    Magic3 Framework
  * @author     平田直毅(Naoki Hirata) <naoki@aplo.co.jp>
- * @copyright  Copyright 2006-2008 Magic3 Project.
+ * @copyright  Copyright 2006-2015 Magic3 Project.
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL License
- * @version    SVN: $Id: clear.inc.php 1098 2008-10-22 11:43:09Z fishbone $
+ * @version    SVN: $Id$
  * @link       http://www.magic3.org
  */
 // Clear plugin - inserts a CSS class 'clear', to set 'clear:both'
 
 function plugin_clear_convert()
 {
-	return '<div class="wiki_clear"></div>';
+	return '<div class="clearfix"></div>';
 }
 ?>
