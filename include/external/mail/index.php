b70465a143d2fdfcce5bf0482c365def076952ca
naoki hirata <naoki@magic3.org>
2016-01-30 09:18:13 +0900

    外部起動インターフェイス追加。

---
+++ include/external/mail/index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * 外部起動用ファイル
+ * index.php
+ *
+ * PHP versions 5
+ *
+ * LICENSE: This source file is licensed under the terms of the GNU General Public License.
+ *
+ * @package    Magic3 Framework
+ * @author     平田直毅(Naoki Hirata) <naoki@aplo.co.jp>
+ * @copyright  Copyright 2006-2016 Magic3 Project.
+ * @license    http://www.gnu.org/copyleft/gpl.html  GPL License
+ * @version    SVN: $Id$
+ * @link       http://www.magic3.org
+ */
+// 直接アクセスの防止
+defined('M3_SYSTEM') or die('Access error: Direct access denied.');
+
+// ジョブ実行
+global $gLaunchManager;
+$gLaunchManager->goJob(__FILE__);
+?>
