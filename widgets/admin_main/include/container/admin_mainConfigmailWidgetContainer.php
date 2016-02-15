0551013660e370a8bbf6dea82cdcbd2add846970
naoki hirata <naoki@magic3.org>
2016-02-15 11:40:15 +0900

    SMTP外部サーバ情報設定画面更新。

--- widgets/admin_main/include/container/admin_mainConfigmailWidgetContainer.php
+++ widgets/admin_main/include/container/admin_mainConfigmailWidgetContainer.php
@@ -17,11 +17,16 @@ require_once($gEnvManager->getCurrentWidgetContainerPath() .	'/admin_mainConfigs
 
 class admin_mainConfigmailWidgetContainer extends admin_mainConfigsystemBaseWidgetContainer
 {
-	private $db;	// DB接続オブジェクト
-	private $langId;		// 変更対象の言語ID
-	const CF_USE_CONTENT_MAINTENANCE = 'use_content_maintenance';		// メンテナンス用コンテンツを汎用コンテンツから取得するかどうか
-	const CF_USE_CONTENT_ACCESS_DENY = 'use_content_access_deny';		// アクセス不可用コンテンツを汎用コンテンツから取得するかどうか
-	const CF_USE_CONTENT_PAGE_NOT_FOUND = 'use_content_page_not_found';		// 存在しないページ画面に汎用コンテンツを使用するかどうか
+	private $encryptType;			// 暗号化タイプ
+	private $encryptTypeArray;		// 暗号化タイプ
+	
+	const CF_SMTP_USE_SERVER	= 'smtp_use_server';	// SMTP外部サーバを使用するかどうか
+	const CF_SMTP_HOST			= 'smtp_host';			// SMTPホスト名
+	const CF_SMTP_PORT			= 'smtp_port';			// SMTPポート番号
+	const CF_SMTP_ENCRYPT_TYPE	= 'smtp_encrypt_type';	// SMTP暗号化タイプ
+	const CF_SMTP_AUTHENTICATION	= 'smtp_authentication';		// SMTP認証
+	const CF_SMTP_ACCOUNT		= 'smtp_account';		// SMTP接続アカウント
+	const CF_SMTP_PASSWORD		= 'smtp_password';		// SMTPパスワード
 	
 	/**
 	 * コンストラクタ
@@ -30,6 +35,12 @@ class admin_mainConfigmailWidgetContainer extends admin_mainConfigsystemBaseWidg
 	{
 		// 親クラスを呼び出す
 		parent::__construct();
+		
+		// 暗号化タイプ
+		$this->encryptTypeArray = array(	array(	'name' => 'なし',	'value' => ''),
+											array(	'name' => 'SSL',	'value' => 'ssl'),
+											array(	'name' => 'TLS',	'value' => 'tls')			// デフォルト
+										);
 	}
 	/**
 	 * テンプレートファイルを設定
@@ -56,47 +67,47 @@ class admin_mainConfigmailWidgetContainer extends admin_mainConfigsystemBaseWidg
 	 */
 	function _assign($request, &$param)
 	{
-		// ユーザ情報、表示言語
-		$userInfo		= $this->gEnv->getCurrentUserInfo();
-		$this->langId		= $this->gEnv->getCurrentLanguage();
-
 		$act = $request->trimValueOf('act');
-		$msg_siteInMaintenance = $request->trimValueOf('item_msg_site_in_maintenance');		// メンテナンス中メッセージ
-		$useContentMaintenance = ($request->trimValueOf('item_use_content_maintenance') == 'on') ? 1 : 0;		// メンテナンス画面用コンテンツを汎用コンテンツから取得するかどうか
-		$msg_accessDeny = $request->trimValueOf('item_msg_access_deny');		// アクセス不可メッセージ
-		$useContentAccessDeny = ($request->trimValueOf('item_use_content_access_deny') == 'on') ? 1 : 0;		// アクセス不可用コンテンツを汎用コンテンツから取得するかどうか
-		$msg_pageNotFound = $request->trimValueOf('item_msg_page_not_found');		// 存在しない画面メッセージ
-		$useContentPageNotFound = ($request->trimValueOf('item_use_content_page_not_found') == 'on') ? 1 : 0;		// 存在しない画面用コンテンツを汎用コンテンツから取得するかどうか
 		
+		$useServer	= $request->trimCheckedValueOf('item_smtp_use_server');			// SMTP外部サーバを使用するかどうか
+		$host		= $request->trimValueOf('item_smtp_host');			// SMTPホスト名
+		$port		= $request->trimValueOf('item_smtp_port');			// SMTPポート番号
+		$this->encryptType	= $request->trimValueOf('item_smtp_encrypt_type');		// 暗号化タイプ
+		$authentication = $request->trimCheckedValueOf('item_smtp_authentication');		// SMTP認証
+		$account	= $request->trimValueOf('item_smtp_account');		// SMTP接続アカウント
+		$password	= $request->trimValueOf('item_smtp_password');		// SMTPパスワード
+	
 		$replaceNew = false;		// データを再取得するかどうか
 		if ($act == 'update'){		// 設定更新のとき
 			$isErr = false;
+
 			if (!$isErr){
-				if (!$this->gInstance->getMessageManager()->updateMessage(MessageManager::MSG_SITE_IN_MAINTENANCE, $msg_siteInMaintenance, $this->langId)) $isErr = true;
+				if (!$this->_mainDb->updateSystemConfig(self::CF_SMTP_USE_SERVER, $useServer)) $isErr = true;// SMTP外部サーバを使用するかどうか
 			}
 			if (!$isErr){
-				if (!$this->_mainDb->updateSystemConfig(self::CF_USE_CONTENT_MAINTENANCE, $useContentMaintenance)) $isErr = true;		// メンテナンス画面用コンテンツを汎用コンテンツから取得するかどうか
+				if (!$this->_mainDb->updateSystemConfig(self::CF_SMTP_HOST, $host)) $isErr = true;// SMTPホスト名
 			}
 			if (!$isErr){
-				if (!$this->gInstance->getMessageManager()->updateMessage(MessageManager::MSG_ACCESS_DENY, $msg_accessDeny, $this->langId)) $isErr = true;
+				if (!$this->_mainDb->updateSystemConfig(self::CF_SMTP_PORT, $port)) $isErr = true;// SMTPポート番号
 			}
 			if (!$isErr){
-				if (!$this->_mainDb->updateSystemConfig(self::CF_USE_CONTENT_ACCESS_DENY, $useContentAccessDeny)) $isErr = true;		// アクセス不可画面用コンテンツを汎用コンテンツから取得するかどうか
+				if (!$this->_mainDb->updateSystemConfig(self::CF_SMTP_ENCRYPT_TYPE, $this->encryptType)) $isErr = true;// 暗号化タイプ
 			}
 			if (!$isErr){
-				if (!$this->gInstance->getMessageManager()->updateMessage(MessageManager::MSG_PAGE_NOT_FOUND, $msg_pageNotFound, $this->langId)) $isErr = true;
+				if (!$this->_mainDb->updateSystemConfig(self::CF_SMTP_AUTHENTICATION, $authentication)) $isErr = true;// SMTP認証
 			}
 			if (!$isErr){
-				if (!$this->_mainDb->updateSystemConfig(self::CF_USE_CONTENT_PAGE_NOT_FOUND, $useContentPageNotFound)) $isErr = true;		// 存在しない画面用コンテンツを汎用コンテンツから取得するかどうか
+				if (!$this->_mainDb->updateSystemConfig(self::CF_SMTP_ACCOUNT, $account)) $isErr = true;// SMTP接続アカウント
 			}
+			if (!$isErr){
+				if (!$this->_mainDb->updateSystemConfig(self::CF_SMTP_PASSWORD, $password)) $isErr = true;// SMTPパスワード
+			}
+	
 			if ($isErr){		// エラー発生のとき
 				$this->setMsg(self::MSG_APP_ERR, 'データ更新に失敗しました');
 			} else {
 				$this->setMsg(self::MSG_GUIDANCE, 'データを更新しました');
-				
-				// 再読み込みフラグをセット
-				$this->gInstance->getMessageManager()->reloadMessage();
-				
+			
 				$replaceNew = true;		// データを再取得
 			}
 		} else {
@@ -104,32 +115,45 @@ class admin_mainConfigmailWidgetContainer extends admin_mainConfigsystemBaseWidg
 		}
 		
 		if ($replaceNew){
-			$msg_siteInMaintenance = $this->gInstance->getMessageManager()->getMessage(MessageManager::MSG_SITE_IN_MAINTENANCE, $this->langId);// メンテナンス中メッセージ
-			$useContentMaintenance	= $this->_mainDb->getSystemConfig(self::CF_USE_CONTENT_MAINTENANCE);			// メンテナンス画面用コンテンツを汎用コンテンツから取得するかどうか
-			$msg_accessDeny = $this->gInstance->getMessageManager()->getMessage(MessageManager::MSG_ACCESS_DENY, $this->langId);// アクセス不可メッセージ
-			$useContentAccessDeny	= $this->_mainDb->getSystemConfig(self::CF_USE_CONTENT_ACCESS_DENY);			// アクセス不可用コンテンツを汎用コンテンツから取得するかどうか
-			$msg_pageNotFound = $this->gInstance->getMessageManager()->getMessage(MessageManager::MSG_PAGE_NOT_FOUND, $this->langId);// 存在しない画面メッセージ
-			$useContentPageNotFound	= $this->_mainDb->getSystemConfig(self::CF_USE_CONTENT_PAGE_NOT_FOUND);			// 存在しない画面用コンテンツを汎用コンテンツから取得するかどうか
+			$useServer		= $this->_mainDb->getSystemConfig(self::CF_SMTP_USE_SERVER);			// SMTP外部サーバを使用するかどうか
+			$host			= $this->_mainDb->getSystemConfig(self::CF_SMTP_HOST);			// SMTPホスト名
+			$port			= $this->_mainDb->getSystemConfig(self::CF_SMTP_PORT);			// SMTPポート番号
+			$this->encryptType = $this->_mainDb->getSystemConfig(self::CF_SMTP_ENCRYPT_TYPE);			// 暗号化タイプ
+			$authentication	= $this->_mainDb->getSystemConfig(self::CF_SMTP_AUTHENTICATION);			// SMTP認証
+			$account		= $this->_mainDb->getSystemConfig(self::CF_SMTP_ACCOUNT);			// SMTP接続アカウント
+			$password		= $this->_mainDb->getSystemConfig(self::CF_SMTP_PASSWORD);			// SMTPパスワード
 		}
 		
+		// 暗号化タイプメニュー作成
+		$this->createEncryptTypeMenu();
+		
 		// 画面にデータを埋め込む
-		$this->tmpl->addVar("_widget", "msg_site_in_maintenance", $msg_siteInMaintenance);// メンテナンスメッセージ
-		$this->tmpl->addVar("_widget", "content_key_maintenance", M3_CONTENT_KEY_MAINTENANCE);		// メンテナンス用コンテンツ取得キー
-		$checked = '';
-		if (!empty($useContentMaintenance)) $checked = 'checked';
-		$this->tmpl->addVar("_widget", "use_content_maintenance", $checked);// メンテナンス画面用コンテンツを汎用コンテンツから取得するかどうか
+		$this->tmpl->addVar('_widget', 'smtp_use_server',	$this->convertToCheckedString($useServer));		// SMTP外部サーバを使用するかどうか
+		$this->tmpl->addVar('_widget', 'smtp_host',			$this->convertToDispString($host));			// SMTPホスト名
+		$this->tmpl->addVar('_widget', 'smtp_port',			$this->convertToDispString($port));			// SMTPポート番号
+		$this->tmpl->addVar('_widget', 'smtp_authentication', $this->convertToCheckedString($authentication));	// SMTP認証
+		$this->tmpl->addVar('_widget', 'smtp_account',		$this->convertToDispString($account));				// SMTP接続アカウント
+		$this->tmpl->addVar('_widget', 'smtp_password',		$this->convertToDispString($password));				// SMTPパスワード
+	}
+	/**
+	 * 暗号化タイプ選択メニュー作成
+	 *
+	 * @return なし
+	 */
+	function createEncryptTypeMenu()
+	{
+		for ($i = 0; $i < count($this->encryptTypeArray); $i++){
+			$value = $this->encryptTypeArray[$i]['value'];
+			$name = $this->encryptTypeArray[$i]['name'];
 		
-		$this->tmpl->addVar("_widget", "msg_access_deny", $msg_accessDeny);				// アクセス不可メッセージ
-		$this->tmpl->addVar("_widget", "content_key_access_deny", M3_CONTENT_KEY_ACCESS_DENY);		// アクセス不可用コンテンツ取得キー
-		$checked = '';
-		if (!empty($useContentAccessDeny)) $checked = 'checked';
-		$this->tmpl->addVar("_widget", "use_content_access_deny", $checked);// アクセス不可用コンテンツを汎用コンテンツから取得するかどうか
-		
-		$this->tmpl->addVar("_widget", "msg_page_not_found", $msg_pageNotFound);				// 存在しない画面メッセージ
-		$this->tmpl->addVar("_widget", "content_key_page_not_found", M3_CONTENT_KEY_PAGE_NOT_FOUND);		// 存在しない画面用コンテンツ取得キー
-		$checked = '';
-		if (!empty($useContentPageNotFound)) $checked = 'checked';
-		$this->tmpl->addVar("_widget", "use_content_page_not_found", $checked);// 存在しない画面用コンテンツを汎用コンテンツから取得するかどうか
+			$row = array(
+				'value'    => $this->convertToDispString($value),			// 値
+				'name'     => $this->convertToDispString($name),			// 名前
+				'selected' => $this->convertToSelectedString($value, $this->encryptType)			// 選択中かどうか
+			);
+			$this->tmpl->addVars('encrypt_type_list', $row);
+			$this->tmpl->parseTemplate('encrypt_type_list', 'a');
+		}
 	}
 }
 ?>
