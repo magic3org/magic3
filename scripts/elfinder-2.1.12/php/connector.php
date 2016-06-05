8179028e4e23d374b4fa1f9e3bd8500be8f8d0c7
naoki hirata <naoki@magic3.org>
2016-06-05 23:00:52 +0900

    elFinder更新。

--- scripts/elfinder-2.1.12/php/connector.php
+++ scripts/elfinder-2.1.12/php/connector.php
@@ -1,6 +1,29 @@
 <?php
+/**
+ * index.php
+ *
+ * PHP versions 5
+ *
+ * LICENSE: This source file is licensed under the terms of the GNU General Public License.
+ *
+ * @package    Magic3 Framework
+ * @author     平田直毅(Naoki Hirata) <naoki@aplo.co.jp>
+ * @copyright  Copyright 2006-2016 Magic3 Project.
+ * @license    http://www.gnu.org/copyleft/gpl.html  GPL License
+ * @version    SVN: $Id$
+ * @link       http://www.magic3.org
+ */
+// ########## Magic3アクセス制御(開始) ##########
+require_once('../../../include/global.php');
 
-error_reporting(0); // Set E_ALL for debuging
+if (!$gAccessManager->loginedByUser()){		// ログイン中のユーザはアクセスを許可
+	echo 'Access error: access denied.';
+
+	$gOpeLogManager->writeUserAccess(__METHOD__, 'ファイルブラウザへの不正なアクセスを検出しました。ログインなし', 3001, 'アクセスをブロックしました。');
+	exit(0);
+}
+// ########## Magic3アクセス制御(終了) ##########
+//error_reporting(0); // Set E_ALL for debuging
 
 // load composer autoload before load elFinder autoload If you need composer
 //require './vendor/autoload.php';
@@ -10,7 +33,7 @@ require './autoload.php';
 // ===============================================
 
 // Enable FTP connector netmount
-elFinder::$netDrivers['ftp'] = 'FTP';
+//elFinder::$netDrivers['ftp'] = 'FTP';
 // ===============================================
 
 /**
@@ -58,6 +81,28 @@ function access($attr, $path, $data, $volume) {
 		:  null;                                    // else elFinder decide it itself
 }
 
+// ########## Magic3アクセス制御(開始) ##########
+// ディレクトリ参照範囲を制限
+$dirType = $gRequestManager->trimValueOf('dirtype');
+if (!empty($dirType) && !in_array($dirType, array('image', 'flash', 'file'))){
+	$gOpeLogManager->writeUserAccess(__METHOD__, 'ファイルブラウザへの不正なパラメータを検出しました。dirtype=' . $dirType , 3001, 'アクセスをブロックしました。');
+	exit(0);
+}
+$path = $gEnvManager->getResourcePathForUser();
+$url = $gEnvManager->getRelativeResourcePathToDocumentRootForUser();
+if (!empty($dirType)){
+	$path .= '/' . $dirType;
+	$url .= '/' . $dirType;
+}
+
+// ディレクトリがない場合は作成
+if (!file_exists($path)) mkdir($path, M3_SYSTEM_DIR_PERMISSION, true/*再帰的*/);
+// ########## Magic3アクセス制御(終了) ##########
+
+// 画像の自動生成の設定
+$autoResizeEnable	= (bool)$gSystemManager->getSystemConfig(SystemManager::CF_UPLOAD_IMAGE_AUTORESIZE);		// 画像リサイズ機能を使用するかどうか
+$maxWidth			= $gSystemManager->getSystemConfig(SystemManager::CF_UPLOAD_IMAGE_AUTORESIZE_MAX_WIDTH);		// 画像リサイズ機能最大画像幅
+$maxHeight			= $gSystemManager->getSystemConfig(SystemManager::CF_UPLOAD_IMAGE_AUTORESIZE_MAX_HEIGHT);		// 画像リサイズ機能最大画像高さ
 
 // Documentation for connector options:
 // https://github.com/Studio-42/elFinder/wiki/Connector-configuration-options
@@ -65,13 +110,23 @@ $opts = array(
 	// 'debug' => true,
 	'roots' => array(
 		array(
-			'driver'        => 'LocalFileSystem',           // driver for accessing file system (REQUIRED)
-			'path'          => '../files/',                 // path to files (REQUIRED)
-			'URL'           => dirname($_SERVER['PHP_SELF']) . '/../files/', // URL to files (REQUIRED)
-			'uploadDeny'    => array('all'),                // All Mimetypes not allowed to upload
-			'uploadAllow'   => array('image', 'text/plain'),// Mimetype `image` and `text/plain` allowed to upload
-			'uploadOrder'   => array('deny', 'allow'),      // allowed Mimetype `image` and `text/plain` only
-			'accessControl' => 'access'                     // disable and hide dot starting files (OPTIONAL)
+			'driver'        => 'LocalFileSystem',   // driver for accessing file system (REQUIRED)
+			'path'          => $path,		// path to files (REQUIRED)
+			'URL'           => $url,		// URL to files (REQUIRED)
+			'accessControl' => 'access'             // disable and hide dot starting files (OPTIONAL)
+		)
+	),
+	'bind' =>	array(
+		'upload.presave' => array(
+			'Plugin.AutoResize.onUpLoadPreSave'
+		)
+	),
+	'plugin' => array(
+		'AutoResize' => array(
+			'enable'		=> $autoResizeEnable,		// 画像サイズを制限
+			'maxWidth'		=> $maxWidth,		// 最大画像幅
+			'maxHeight'		=> $maxHeight,		// 最大画像高さ
+			'quality'		=> 100			// JPEG image save quality
 		)
 	)
 );
