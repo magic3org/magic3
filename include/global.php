52fb48d2396b453d85d23b74cc78ce36c5846b3f
naoki hirata <naoki@magic3.org>
2017-08-20 16:59:41 +0900

    バージョン番号更新。

--- include/global.php
+++ include/global.php
@@ -12,7 +12,7 @@
  * @author     平田直毅(Naoki Hirata) <naoki@aplo.co.jp>
  * @copyright  Copyright 2006-2017 Magic3 Project.
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL License
- * @version    Release 2.20.x SVN: $Id$
+ * @version    Release 2.21.x SVN: $Id$
  * @link       http://www.magic3.org
  */
 if (defined("M3_SYSTEM_DEFINE_INCLUDED")) return;
@@ -57,7 +57,7 @@ define('M3_TITLE_BRACKET_END',		'」');					// タイトルを括弧表記する
 // ########## システム関係 ##########
 // システム基本情報
 define('M3_SYSTEM_NAME',			'Magic3');		// システム名称
-define('M3_SYSTEM_VERSION',			'2.20.6');		// システムのバージョン
+define('M3_SYSTEM_VERSION',			'2.21.0');		// システムのバージョン
 define('M3_SYSTEM_RELEASE_DATE',	'2017/8/12');	// システムのリリース日付
 define('M3_SYSTEM_ID',				'M3');			// コンポーネント識別用ID
 define('M3_SYSTEM_DESCRIPTION',		'Magic3 is open source cms and web communication platform.');		// システムの説明
