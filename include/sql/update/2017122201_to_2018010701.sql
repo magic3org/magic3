c3f0d474c439e3e7ded8abcbaa25ec5b4184727d
naoki hirata <naoki@magic3.org>
2018-01-07 20:03:53 +0900

    DB更新スクリプト追加。

---
+++ include/sql/update/2017122201_to_2018010701.sql
@@ -0,0 +1,27 @@
+-- *
+-- * バージョンアップ用スクリプト
+-- *
+-- * PHP versions 5
+-- *
+-- * LICENSE: This source file is licensed under the terms of the GNU General Public License.
+-- *
+-- * @package    Magic3 Framework
+-- * @author     平田直毅(Naoki Hirata) <naoki@aplo.co.jp>
+-- * @copyright  Copyright 2006-2017 Magic3 Project.
+-- * @license    http://www.gnu.org/copyleft/gpl.html  GPL License
+-- * @version    SVN: $Id$
+-- * @link       http://www.magic3.org
+-- *
+-- --------------------------------------------------------------------------------------------------
+-- バージョンアップ用スクリプト
+-- --------------------------------------------------------------------------------------------------
+
+-- *** システムベーステーブル ***
+-- テンプレート情報
+ALTER TABLE _templates ADD tm_editor_param               TEXT                                         NOT NULL;      -- 編集エディタ用パラメータ
+
+DELETE FROM _templates WHERE tm_id = 'themler_sample1';
+DELETE FROM _templates WHERE tm_id = 'themler_sample2';
+
+
+-- *** システム標準テーブル ***
