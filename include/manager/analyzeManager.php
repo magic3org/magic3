5ce8ce401137008ac311e80e822efd226a2bbb28
naoki hirata <naoki@magic3.org>
2018-06-03 21:50:09 +0900

    クローラー情報追加。

--- include/manager/analyzeManager.php
+++ include/manager/analyzeManager.php
@@ -8,7 +8,7 @@
  *
  * @package    Magic3 Framework
  * @author     平田直毅(Naoki Hirata) <naoki@aplo.co.jp>
- * @copyright  Copyright 2006-2017 Magic3 Project.
+ * @copyright  Copyright 2006-2018 Magic3 Project.
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL License
  * @version    SVN: $Id$
  * @link       http://www.magic3.org
@@ -552,6 +552,7 @@ $agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like G
 			'scej psp browser'				=> '',	// ワイプアウトピュア
 			'w3m'							=> '',
 			'netcaptor'						=> 'netcaptor.gif',
+			'netcraft'						=> 'netcraft.png',
 			'webtv'							=> 'webtv.gif',
 			'vivaldi'						=> 'vivaldi.png',
 			
