33ffa88dceb1689b6750f5166b1b07334dad17bf
naoki hirata <naoki@magic3.org>
2018-09-12 06:07:23 +0900

    DB更新スクリプト追加。

---
+++ include/sql/update/2018080701_to_2018091201.sql
@@ -0,0 +1,23 @@
+-- *
+-- * バージョンアップ用スクリプト
+-- *
+-- * PHP versions 5
+-- *
+-- * LICENSE: This source file is licensed under the terms of the GNU General Public License.
+-- *
+-- * @package    Magic3 Framework
+-- * @author     平田直毅(Naoki Hirata) <naoki@aplo.co.jp>
+-- * @copyright  Copyright 2006-2018 Magic3 Project.
+-- * @license    http://www.gnu.org/copyleft/gpl.html  GPL License
+-- * @version    SVN: $Id$
+-- * @link       http://www.magic3.org
+-- *
+-- --------------------------------------------------------------------------------------------------
+-- バージョンアップ用スクリプト
+-- --------------------------------------------------------------------------------------------------
+
+-- *** システムベーステーブル ***
+-- ウィジェット情報マスター
+ALTER TABLE _widgets ADD wd_hide_title           BOOLEAN        DEFAULT false                 NOT NULL;      -- 実稼働時に強制的にタイトルを非表示にするかどうか
+
+-- *** システム標準テーブル ***
