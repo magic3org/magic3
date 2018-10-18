d8af3569c0323ed6b81b01efb2c0785c32eda3d4
naoki hirata <naoki@magic3.org>
2018-10-18 19:09:56 +0900

    Google Analytics(gtag.js)ウィジェット追加。

---
+++ widgets/g_analytics2/include/container/admin_g_analytics2WidgetContainer.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * コンテナクラス
+ *
+ * PHP versions 5
+ *
+ * LICENSE: This source file is licensed under the terms of the GNU General Public License.
+ *
+ * @package    Magic3 Framework
+ * @author     平田直毅(Naoki Hirata) <naoki@aplo.co.jp>
+ * @copyright  Copyright 2006-2018 Magic3 Project.
+ * @license    http://www.gnu.org/copyleft/gpl.html  GPL License
+ * @version    SVN: $Id$
+ * @link       http://www.magic3.org
+ */
+require_once($gEnvManager->getContainerPath() . '/baseAdminWidgetContainer.php');
+
+class admin_g_analytics2WidgetContainer extends BaseAdminWidgetContainer
+{
+	/**
+	 * コンストラクタ
+	 */
+	function __construct()
+	{
+		// 親クラスを呼び出す
+		parent::__construct();
+	}
+	/**
+	 * テンプレートファイルを設定
+	 *
+	 * _assign()でデータを埋め込むテンプレートファイルのファイル名を返す。
+	 * 読み込むディレクトリは、「自ウィジェットディレクトリ/include/template」に固定。
+	 *
+	 * @param RequestManager $request		HTTPリクエスト処理クラス
+	 * @param object         $param			任意使用パラメータ。そのまま_assign()に渡る
+	 * @return string 						テンプレートファイル名。テンプレートライブラリを使用しない場合は空文字列「''」を返す。
+	 */
+	function _setTemplate($request, &$param)
+	{	
+		return 'admin.tmpl.html';
+	}
+	/**
+	 * テンプレートにデータ埋め込む
+	 *
+	 * _setTemplate()で指定したテンプレートファイルにデータを埋め込む。
+	 *
+	 * @param RequestManager $request		HTTPリクエスト処理クラス
+	 * @param object         $param			任意使用パラメータ。_setTemplate()と共有。
+	 * @param								なし
+	 */
+	function _assign($request, &$param)
+	{
+		$act = $request->trimValueOf('act');
+		if ($act == 'update'){		// 設定更新のとき
+			// 入力値を取得
+			$account	= $request->valueOf('account_no');			// Google Analyticsアカウント番号
+			
+			if ($this->getMsgCount() == 0){			// エラーのないとき
+				$paramObj = new stdClass;
+				$paramObj->account	= $account;
+				$ret = $this->updateWidgetParamObj($paramObj);
+				if ($ret){
+					$this->setMsg(self::MSG_GUIDANCE, 'データを更新しました');
+				} else {
+					$this->setMsg(self::MSG_APP_ERR, 'データ更新に失敗しました');
+				}
+				$this->gPage->updateParentWindow();// 親ウィンドウを更新
+			}
+		} else {		// 初期表示の場合
+			// デフォルト値設定
+			$account = '';	// Google Analyticsアカウント番号
+			$paramObj = $this->getWidgetParamObj();
+			if (!empty($paramObj)){
+				$account	= $paramObj->account;
+			}
+		}
+		
+		// 画面にデータを埋め込む
+		$this->tmpl->addVar("_widget", "account_no",	$account);
+	}
+	/**
+	 * テンプレートにデータ埋め込む
+	 *
+	 * _setTemplate()で指定したテンプレートファイルにデータを埋め込む。
+	 *
+	 * @param RequestManager $request		HTTPリクエスト処理クラス
+	 * @param object         $param			任意使用パラメータ。_setTemplate()と共有。
+	 * @return								なし
+	 */
+	function _postAssign($request, &$param)
+	{
+		// メニューバー、パンくずリスト作成(簡易版)
+		$this->createBasicConfigMenubar($request);
+	}
+}
+?>
---
+++ widgets/g_analytics2/include/container/g_analytics2WidgetContainer.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * index.php用コンテナクラス
+ *
+ * PHP versions 5
+ *
+ * LICENSE: This source file is licensed under the terms of the GNU General Public License.
+ *
+ * @package    Magic3 Framework
+ * @author     平田直毅(Naoki Hirata) <naoki@aplo.co.jp>
+ * @copyright  Copyright 2006-2018 Magic3 Project.
+ * @license    http://www.gnu.org/copyleft/gpl.html  GPL License
+ * @version    SVN: $Id$
+ * @link       http://www.magic3.org
+ */
+require_once($gEnvManager->getContainerPath() . '/baseWidgetContainer.php');
+
+class g_analytics2WidgetContainer extends BaseWidgetContainer
+{
+	private $account;		// Google AnalyticsのプロファイルのID
+	private $script;		// ヘッダ用javascript
+	
+	/**
+	 * コンストラクタ
+	 */
+	function __construct()
+	{
+		// 親クラスを呼び出す
+		parent::__construct();
+	}
+	/**
+	 * テンプレートファイルを設定
+	 *
+	 * _assign()でデータを埋め込むテンプレートファイルのファイル名を返す。
+	 * 読み込むディレクトリは、「自ウィジェットディレクトリ/include/template」に固定。
+	 *
+	 * @param RequestManager $request		HTTPリクエスト処理クラス
+	 * @param object         $param			任意使用パラメータ。そのまま_assign()に渡る
+	 * @return string 						テンプレートファイル名。テンプレートライブラリを使用しない場合は空文字列「''」を返す。
+	 */
+	function _setTemplate($request, &$param)
+	{
+		return '';
+	}
+	/**
+	 * テンプレートにデータ埋め込む
+	 *
+	 * _setTemplate()で指定したテンプレートファイルにデータを埋め込む。
+	 *
+	 * @param RequestManager $request		HTTPリクエスト処理クラス
+	 * @param object         $param			任意使用パラメータ。_setTemplate()と共有。
+	 * @param								なし
+	 */
+	function _assign($request, &$param)
+	{
+		$this->account = '';	// Google AnalyticsのプロファイルのID
+		$paramObj = $this->getWidgetParamObj();
+		if (!empty($paramObj)){
+			$this->account	= $paramObj->account;
+		}
+
+		// ヘッダ用Javascript作成
+		// アカウント番号が空のときは出力しない
+		$this->script = '';
+		if (!empty($this->account)) $this->script = $this->getParsedTemplateData('default.tmpl.js', array($this, 'makeScript'));
+	}
+	/**
+	 * JavascriptをHTMLヘッダ部に設定
+	 *
+	 * CSSデータをHTMLのheadタグ内に追加出力する。
+	 * _assign()よりも後に実行される。
+	 *
+	 * @param RequestManager $request		HTTPリクエスト処理クラス
+	 * @param object         $param			任意使用パラメータ。
+	 * @return string 						CSS文字列。出力しない場合は空文字列を設定。
+	 */
+	function _addScriptToHead($request, &$param)
+	{
+		return $this->script;
+	}
+	/**
+	 * Javascriptデータ作成処理コールバック
+	 *
+	 * @param object         $tmpl			テンプレートオブジェクト
+	 * @param								なし
+	 */
+	function makeScript($tmpl)
+	{
+		$tmpl->addVar("_tmpl", "account_no",	$this->account);		// Google AnalyticsのプロファイルのID
+	}
+}
+?>
