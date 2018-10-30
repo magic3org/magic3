ecbe005e6b60cf4c231413eaa10497682fff234e
naoki hirata <naoki@magic3.org>
2018-10-30 10:51:59 +0900

    システム基本設定画面更新。

--- widgets/admin_main/include/help/help_configsys.php
+++ widgets/admin_main/include/help/help_configsys.php
@@ -9,7 +9,7 @@
  *
  * @package    Magic3 Framework
  * @author     平田直毅(Naoki Hirata) <naoki@aplo.co.jp>
- * @copyright  Copyright 2006-2014 Magic3 Project.
+ * @copyright  Copyright 2006-2018 Magic3 Project.
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL License
  * @version    SVN: $Id$
  * @link       http://www.magic3.org
@@ -34,13 +34,37 @@ class help_configsys extends HelpConv
 			'configsys_ssl_url' => array(	
 				'title' =>	$this->_('Root URL of Shared SSL'),	// 共有SSLのルートURL
 				'body' =>	$this->_('If you use SSL on admin access point or front access point, you can set another url for SSL. If you set blank for the url, the system url which is changed http to https is used.')		// 管理画面またはフロント画面でSSLを使用する場合にSSL専用の別のURLが設定可能です。空に設定した場合はシステムのルートURLをhttpsに変更したURLが使用されます。
+			),
+			'configsys_access_point_in_public' => array(	
+				'title' =>	'アクセスポイント公開状況',
+				'body' =>	'アクセスポイントごとの公開状況を設定します。「公開」にチェックが入っていない場合はアクセス不可のメッセージ画面が表示されます。'
+			),
+			'configsys_admin_mode' => array(	
+				'title' =>	'管理画面モード',
+				'body' =>	'管理画面の運用モードを変更して、メインメニューに表示される項目を変更します。この画面を再表示させるにはダッシュボード画面上でESCキーを押した後に表示されるメニューからアクセスします。'
+			),
+			'configsys_access_point' => array(	
+				'title' =>	'アクセスポイント',
+				'body' =>	'使用するアクセスポイントを有効にします。'
+			),
+			'configsys_network' => array(	
+				'title' =>	'ネットワーク',	// ネットワーク
+				'body' =>	'ネットワークに関するシステムの運用形態を設定します。<br />●イントラネット運用 ー Googleマップ等の外部インターネットサービスを使用しない場合はチェックを入れます。'
+			),
+			'configsys_admin_page' => array(	
+				'title' =>	'管理画面',
+				'body' =>	'利用形態に合わせて管理画面の表示方法を変更します。<br />●マルチデバイス最適化 ― 管理画面をスマートフォン等の画面サイズが小さいデバイス用の表示に自動切り替えします。<br />●詳細設定表示(開発モード) ― システム開発時に使用します。'
+			),
+			'configsys_menu' => array(	
+				'title' =>	'メニュー',
+				'body' =>	'フロントで表示するメニューの単階層、多階層の区別を設定します。管理画面がメニュー階層に合わせて変更されます。'
+			),
+			'configsys_template' => array(	
+				'title' =>	'テンプレート',
+				'body' =>	'テンプレートを選択します。<br />●システム画面テンプレート ― フロントでアクセス不可等のメッセージを表示するシステム画面で使用するテンプレートです。'
 			)
 		);
 		return $helpData;
 	}
-/*
-$HELP['configsys_connect_server_url']['title'] = 'ポータルサーバのURL';
-$HELP['configsys_connect_server_url']['body'] = 'ポータルサーバに接続する場合の接続先URLです。Magic3がインストールされているURLを指定します。';
-*/
 }
 ?>
