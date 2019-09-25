13d6fd627f36c85bc03de6268b879b8b68dcd650
naoki hirata <naoki@magic3.org>
2019-09-25 16:01:56 +0900

    バージョン番号更新。

--- include/global.php
+++ include/global.php
@@ -58,8 +58,8 @@ define('M3_TITLE_BRACKET_END',		'」');					// タイトルを括弧表記する
 // ########## システム関係 ##########
 // システム基本情報
 define('M3_SYSTEM_NAME',			'Magic3');		// システム名称
-define('M3_SYSTEM_VERSION',			'3.0.5');		// システムのバージョン
-define('M3_SYSTEM_RELEASE_DATE',	'2019/3/13');	// システムのリリース日付
+define('M3_SYSTEM_VERSION',			'3.0.6');		// システムのバージョン
+define('M3_SYSTEM_RELEASE_DATE',	'2019/9/25');	// システムのリリース日付
 define('M3_SYSTEM_ID',				'M3');			// コンポーネント識別用ID
 define('M3_SYSTEM_DESCRIPTION',		'Magic3 is open source cms and web communication platform.');		// システムの説明
 define('M3_SYSTEM_MIN_MEMORY',		'16M');			// 最小メモリ使用量
