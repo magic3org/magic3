fd8cc41e9fedb8cd0c7efa9f1bf46be07229b45f
naoki hirata <naoki@magic3.org>
2020-08-11 07:31:56 +0900

    テストプログラム追加。

---
+++ widgets/admin_main/include/container/test/admin_mainTest_ckeditor5WidgetContainer.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * コンテナクラス
+ *
+ * PHP versions 5
+ *
+ * LICENSE: This source file is licensed under the terms of the GNU General Public License.
+ *
+ * @package    Magic3 Framework
+ * @author     平田直毅(Naoki Hirata) <naoki@aplo.co.jp>
+ * @copyright  Copyright 2006-2020 Magic3 Project.
+ * @license    http://www.gnu.org/copyleft/gpl.html  GPL License
+ * @version    SVN: $Id$
+ * @link       http://www.magic3.org
+ */
+require_once($gEnvManager->getCurrentWidgetContainerPath() .	'/admin_mainBaseWidgetContainer.php');
+
+class admin_mainTest_ckeditor5WidgetContainer extends admin_mainBaseWidgetContainer
+{
+	/**
+	 * コンストラクタ
+	 */
+	function __construct()
+	{
+		// 親クラスを呼び出す
+		parent::__construct();
+	}
+	/**
+	 * テンプレートファイルを設定
+	 *
+	 * _assign()でデータを埋め込むテンプレートファイルのファイル名を返す。
+	 * 読み込むディレクトリは、「自ウィジェットディレクトリ/include/template」に固定。
+	 *
+	 * @param RequestManager $request		HTTPリクエスト処理クラス
+	 * @param object         $param			任意使用パラメータ。そのまま_assign()に渡る
+	 * @return string 						テンプレートファイル名。テンプレートライブラリを使用しない場合は空文字列「''」を返す。
+	 */
+	function _setTemplate($request, &$param)
+	{	
+		return 'test/test_ckeditor5.tmpl.html';
+	}
+	/**
+	 * テンプレートにデータ埋め込む
+	 *
+	 * _setTemplate()で指定したテンプレートファイルにデータを埋め込む。
+	 *
+	 * @param RequestManager $request		HTTPリクエスト処理クラス
+	 * @param object         $param			任意使用パラメータ。_setTemplate()と共有。
+	 * @param								なし
+	 */
+	function _assign($request, &$param)
+	{
+		$langId	= $this->gEnv->getCurrentLanguage();		// 表示言語を取得
+		
+//		$connectorUrl = $this->getUrl($this->gEnv->getScriptsUrl() . self::FILE_BROWSER_PATH);
+		$this->tmpl->addVar('_widget', 'url', $connectorUrl);	// ファイルブラウザ接続先URL
+		$this->tmpl->addVar('_widget', 'lang', $langId);		// 表示言語
+	}
+}
+?>
