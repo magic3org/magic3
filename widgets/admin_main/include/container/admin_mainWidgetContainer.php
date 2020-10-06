500fbd9593552c8bdc7fd21370ae7cfceb11d506
naoki hirata <naoki@magic3.org>
2020-10-06 21:32:14 +0900

    システムバージョンアップ処理更新。

--- widgets/admin_main/include/container/admin_mainWidgetContainer.php
+++ widgets/admin_main/include/container/admin_mainWidgetContainer.php
@@ -257,8 +257,8 @@ class admin_mainWidgetContainer extends admin_mainBaseWidgetContainer
 					} else if ($task == 'pagehead' ||				// ページヘッダ情報
 								$task == 'pagehead_detail'){		// ページヘッダ情報
 						$task = 'pagehead';
-					} else if ($task == 'updateserver'){		// システムバージョンアップ
-						$task = 'updateserver';
+					} else if ($task == 'updatesystem'){		// システムバージョンアップ
+						$task = 'updatesystem';
 					} else if ($task == 'portal'){		// Magic3ポータル
 						$task = 'portal';
 					} else if ($task == 'tenantserver_detail'){		// テナントサーバ管理詳細
@@ -323,7 +323,7 @@ class admin_mainWidgetContainer extends admin_mainBaseWidgetContainer
 						case 'menudef':				// 多階層メニュー定義
 						case 'smenudef':			// 単階層メニュー定義
 						case 'linkinfo':			// 内部リンク情報取得
-						case 'updateserver':		// システムバージョンアップ
+						case 'updatesystem':		// システムバージョンアップ
 						case 'configsite':			// サイト情報
 						case 'pagehead':			// ページヘッダ情報
 						case 'portal':				// Magic3ポータル
