02e7613ebdfc044e5f7671853db192f424df4868
naoki hirata <naoki@magic3.org>
2021-05-30 22:28:36 +0900

    サーバ接続コネクター処理更新。

--- widgets/admin_main/include/container/connector/admin_mainConnector_updateanalyticsWidgetContainer.php
+++ widgets/admin_main/include/container/connector/admin_mainConnector_updateanalyticsWidgetContainer.php
@@ -41,10 +41,10 @@ class admin_mainConnector_updateanalyticsWidgetContainer extends BaseAdminWidget
 	 * @param object         $param			任意使用パラメータ。そのまま_assign()に渡る
 	 * @return string 						テンプレートファイル名。テンプレートライブラリを使用しない場合は空文字列「''」を返す。
 	 */
-	function _setTemplate($request, &$param)
+/*	function _setTemplate($request, &$param)
 	{
 		return 'dbcondition.tmpl.html';
-	}
+	}*/
 	/**
 	 * テンプレートにデータ埋め込む
 	 *
@@ -56,9 +56,20 @@ class admin_mainConnector_updateanalyticsWidgetContainer extends BaseAdminWidget
 	 */
 	function _assign($request, &$param)
 	{
-		$act = $request->trimValueOf('act');
+		// ##### ウィジェット出力処理中断 ######
+		$this->gPage->abortWidget();
+			
+		//$act = $request->trimValueOf('act');
 
 		debug('#do analytics');
+		
+		$messageArray = array();
+			$ret = $this->gInstance->getAnalyzeManager()->updateAnalyticsData($messageArray, self::MAX_CALC_MONTHS * 30/*12ヶ月分集計*/);
+			if ($ret){
+				$this->setMsg(self::MSG_GUIDANCE, $messageArray[0]);
+			} else {
+				$this->setMsg(self::MSG_APP_ERR, $messageArray[0]);
+			}
 	}
 }
 ?>
