7a76f8e3572cfd9253af89b0dfbe09291ad65ac1
naoki hirata <naoki@magic3.org>
2023-02-20 07:37:59 +0900

    テンプレートのバージョンを一旦戻す。

--- templates/art42_sample5/library/Artx/Content/Item.php
+++ templates/art42_sample5/library/Artx/Content/Item.php
@@ -1,18 +1,22 @@
 <?php
-defined('_JEXEC') or die();
+defined('_JEXEC') or die;
 
-Artx::load('Artx_Content_ArticleBase');
+Artx::load("Artx_Content_ArticleBase");
 
 abstract class ArtxContentItem extends ArtxContentArticleBase
 {
     public $isPublished;
-    public $showIcons;
+
     public $printIconVisible;
+
     public $emailIconVisible;
+
     public $editIconVisible;
+
     public $introVisible;
-    public $images;
+
     public $readmore;
+
     public $readmoreLink;
 
     protected function __construct($component, $componentParams, $article, $articleParams)
@@ -27,46 +31,37 @@ abstract class ArtxContentItem extends ArtxContentArticleBase
         $this->images = $this->_buildImages($article, $articleParams);
     }
 
-    private function _buildImages($article, $params)
-    {
-        $images =
-            isset($this->_article->images) && is_string($this->_article->images)
-                ? json_decode($this->_article->images)
-                : null;
-        return [
+    private function _buildImages($article, $params) {
+        $images = (isset($this->_article->images) && is_string($this->_article->images)) ? json_decode($this->_article->images) : null;
+        return array(
             'intro' => $this->_buildImageInfo('intro', $params, $images),
-            'fulltext' => $this->_buildImageInfo('fulltext', $params, $images),
-        ];
+            'fulltext' => $this->_buildImageInfo('fulltext', $params, $images));
     }
 
-    private function _buildImageInfo($type, $params, $images)
-    {
-        $image = ['image' => '', 'float' => '', 'class' => '', 'caption' => '', 'alt' => ''];
-        if (is_null($images)) {
+    private function _buildImageInfo($type, $params, $images) {
+        $image = array('image' => '', 'float' => '', 'class' => '', 'caption' => '', 'alt' => '');
+        if (is_null($images))
             return $image;
-        }
-        $properties = [
+        $properties = array(
             'image' => 'image_' . $type,
             'float' => 'float_' . $type,
             'caption' => 'image_' . $type . '_caption',
-            'alt' => 'image_' . $type . '_alt',
-        ];
+            'alt' => 'image_' . $type . '_alt'
+        );
         if (isset($images->{$properties['image']}) && !empty($images->{$properties['image']})) {
             $image['image'] = $images->{$properties['image']};
             $image['float'] = empty($images->{$properties['float']})
                 ? $params->get($properties['float'])
                 : $images->{$properties['float']};
             $image['class'] = 'img-' . $type . '-' . htmlspecialchars($image['float']);
-            if ($images->{$properties['caption']}) {
+            if ($images->{$properties['caption']})
                 $image['caption'] = htmlspecialchars($images->{$properties['caption']});
-            }
             $image['alt'] = $images->{$properties['alt']};
         }
         return $image;
     }
-
-    private function _buildIconJ3($text, $file, $alt, $wrapUpTooltip = false)
-    {
+    
+    private function _buildIconJ3($text, $file, $alt, $wrapUpTooltip = false) {
         $app = JFactory::getApplication();
         $src = JURI::root(true) . '/templates/' . $app->getTemplate();
         preg_match('/<a[^>]*>(.*?)<\/a>/', $text, $matches);
@@ -81,7 +76,7 @@ abstract class ArtxContentItem extends ArtxContentArticleBase
         }
         return $text;
     }
-
+    
     /**
      * @see $emailIconVisible
      */
@@ -125,42 +120,26 @@ abstract class ArtxContentItem extends ArtxContentArticleBase
      * the unpublished article with <div class="system-unpublished">...</div>.
      * By default, this decoration is applied only to articles in lists.
      */
-    public function beginUnpublishedArticle()
-    {
-        return '<div class="system-unpublished">';
-    }
+    public function beginUnpublishedArticle() { return '<div class="system-unpublished">'; }
 
-    public function endUnpublishedArticle()
-    {
-        return '</div>';
-    }
+    public function endUnpublishedArticle() { return '</div>'; }
 
     public function readmore($readmore, $readmoreLink)
     {
-        return '<p class="readmore">' .
-            artxLinkButton([
-                'classes' => ['a' => 'readon'],
-                'link' => $readmoreLink,
-                'content' => str_replace(' ', '&#160;', $readmore),
-            ]) .
-            '</p>';
+        return '<p class="readmore">' . artxLinkButton(array(
+            'classes' => array('a' => 'readon'),
+            'link' => $readmoreLink,
+            'content' => str_replace(' ', '&#160;', $readmore))) . '</p>';
     }
 
-    public function image($image)
-    {
-        $imgTagAttrs = ['src' => $image['image'], 'alt' => $image['alt']];
+    public function image($image) {
+        $imgTagAttrs = array('src' => $image['image'], 'alt' => $image['alt']);
         if ($image['caption']) {
             $imgTagAttrs['class'] = 'caption';
             $imgTagAttrs['title'] = $image['caption'];
         }
-        return artxTagBuilder(
-            'div',
-            ['class' => $image['class']],
-            artxTagBuilder(
-                'img',
-                ['src' => $image['image'], 'alt' => $image['alt']] +
-                    ($image['caption'] ? ['class' => 'caption', 'title' => $image['caption']] : [])
-            )
-        );
+        return artxTagBuilder('div', array('class' => $image['class']),
+            artxTagBuilder('img', array('src' => $image['image'], 'alt' => $image['alt'])
+                + ($image['caption'] ? array('class' => 'caption', 'title' => $image['caption']) : array())));
     }
 }
